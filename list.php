4fc9903123ab17a344aa1c26dac2a97815078ef9
David Donovan <daviddenovannn@gmail.com>
2025-04-25 10:28:36 +0700

    menampilkan data karyaawan indoternak

---
+++ list.php
@@ -0,0 +1,32 @@
+<?php
+
+include("connection.php");
+
+$query = mysqli_query($connect, "SELECT * FROM indoternak");
+$results = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+// print_r($results);
+?>
+
+<html>
+
+<body>
+    <table border="1">
+        <tr>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Umur</th>
+            <th>Jenis Kelaming</th>
+        </tr>
+        <?php foreach ($results as $result) : ?>
+            <tr>
+                <td><?php echo $result['nama'] ?></td>
+                <td><?php echo $result['alamat'] ?></td>
+                <td><?php echo $result['umur'] ?></td>
+                <td><?php echo $result['jenis_kelamin'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+
+</html>
